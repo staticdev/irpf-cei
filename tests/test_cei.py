ee02c4e9e96d088ac608cf93e7ced5c7844cac7e
staticdev <thiagocavila@gmail.com>
2020-04-15 20:52:21 -0300

    #2 fix tests typing and order

--- tests/test_cei.py
+++ tests/test_cei.py
@@ -20,34 +20,37 @@ def mock_pandas_read_excel(mocker: MockFixture) -> Mock:
     return mocker.patch("pandas.read_excel")
 
 
+def test_read_xls(mock_pandas_read_excel: Mock) -> None:
+    cei.read_xls("my.xls")
+    mock_pandas_read_excel.assert_called_once()
+
+
 def test_round_down() -> None:
     assert cei.round_down(5.999) == 5.99
     assert cei.round_down(8.5) == 8.50
     assert cei.round_down(5.555) == 5.55
 
 
-def test_read_xls(mock_pandas_read_excel) -> None:
-    cei.read_xls("my.xls")
-    mock_pandas_read_excel.assert_called_once()
-
-
 @pytest.fixture
-def cwd(fs, monkeypatch):
+def cwd(fs: MockFixture, monkeypatch: pytest.MonkeyPatch):
     fs.cwd = "/my/path"
     monkeypatch.setenv("HOME", "/home/user")
 
 
-def test_get_xls_filename_not_found(fs, cwd) -> None:
+def test_get_xls_filename_not_found(fs: MockFixture, cwd: Mock) -> None:
+    """It raises `SystemExit` when file is not found."""
     with pytest.raises(SystemExit):
         assert cei.get_xls_filename()
 
 
-def test_get_xls_filename_current_folder(fs, cwd) -> None:
+def test_get_xls_filename_current_folder(fs: MockFixture, cwd: Mock) -> None:
+    """It returns filename found in current folder."""
     fs.create_file("/my/path/InfoCEI.xls")
     assert cei.get_xls_filename() == "InfoCEI.xls"  # adapted to your implementation
 
 
-def test_get_xls_filename_download_folder(fs, cwd) -> None:
+def test_get_xls_filename_download_folder(fs: MockFixture, cwd: Mock) -> None:
+    """It returns filename found in downloads folder."""
     path = os.path.join("/home/user", "Downloads", "InfoCEI.xls")
     fs.create_file(path)
     assert cei.get_xls_filename() == path
