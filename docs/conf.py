bc7a7d472fc175007350e3ff8c5dc66e288f7906
staticdev <thiagocavila@gmail.com>
2020-06-18 19:55:05 -0300

    nox-mypy

---
+++ docs/conf.py
@@ -0,0 +1,9 @@
+"""Sphinx configuration."""
+from datetime import datetime
+
+
+project = "IRPF CEI"
+author = "Thiago Carvalho D'Ávila"
+copyright = f"{datetime.now().year}, {author}"
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
+autodoc_typehints = "description"
